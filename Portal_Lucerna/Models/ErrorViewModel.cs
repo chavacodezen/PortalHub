253b70732b9a3b0df7f816c515601221bb3eb84f
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2024-03-06 10:24:36 -0700

    .NET Project creation

---
+++ Portal_Lucerna/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portal_Lucerna.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
