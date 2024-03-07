c796787bab8a4f31dd2210a3a66a143ab45139df
Bryan CHAVARRIA <b.chavarria@lacroix.group>
2024-03-07 09:29:04 -0700

    Add Logo to project

--- PortalHub/obj/Debug/net8.0/PortalHub.AssemblyInfo.cs
+++ PortalHub/obj/Debug/net8.0/PortalHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3569a84ee44b169c9be6e0e17f3906dced1eae39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03bbe4787119a0a0cd3ed2f05494a20494b11c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
